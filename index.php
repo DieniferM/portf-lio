9adf8727a62c3ade20b7560635ea11b062a193ed
DieniferM <dienifer92@hotmail.com>
2025-01-19 14:00:07 -0300

    cambia foto

--- index.php
+++ index.php
@@ -8,19 +8,33 @@ $translations = [
                         o que tem me proporcionado uma nova perspectiva sobre o mercado de tecnologia. Sempre fui apaixonada 
                         por tecnologias e videogames, o que me levou a seguir a carreira de desenvolvedora. Acredito firmemente 
                         que o conhecimento é poder, e que todos podemos aprender o que quisermos e nos tornar o que desejamos.',
+        'home'      => 'Quem sou eu',
+        'projects'  => 'Projetos',
+        'contact'   => 'Contato',
+        'technologies' => 'Tecnologias',
+        'linkedin'  => 'Visite meu LinkedIn',
     ],
     'en' => [
         'biografia' => 'I am a Full Stack developer with 2 years of experience and currently living in Italy,
-         which has given me a new perspective on the technology market. I have always been passionate
-          about technology and video games, which led me to pursue a career as a developer. I firmly believe
-           that knowledge is power, and that we can all learn whatever we want and become whatever we desire',
+                        which has given me a new perspective on the technology market. I have always been passionate
+                        about technology and video games, which led me to pursue a career as a developer. I firmly believe
+                        that knowledge is power, and that we can all learn whatever we want and become whatever we desire',
+        'home'      => 'Who am I',
+        'projects'  => 'Projects',
+        'contact'   => 'Contact',
+        'technologies' => 'Technologies',
+        'linkedin'  => 'Visit my LinkedIn',
     ],
     'it' => [
         'biografia' => 'Sono una programmatrice Full Stack con 2 anni di esperienza e attualmente vivo in Italia,
-         il che mi sta dando una nuova prospettiva sul mercato della tecnologia. Sono sempre stata appassionata
-          di tecnologie e videogiochi, il che mi ha portato a seguire la carriera di sviluppatrice. Credo fermamente 
-          che la conoscenza sia potere, e che tutti possiamo imparare ciò che vogliamo e diventare ciò che desideriamo.',
-       
+                        il che mi sta dando una nuova prospettiva sul mercato della tecnologia. Sono sempre stata appassionata
+                        di tecnologie e videogiochi, il che mi ha portato a seguire la carriera di sviluppatrice. Credo fermamente 
+                        che la conoscenza sia potere, e che tutti possiamo imparare ciò che vogliamo e diventare ciò che desideriamo.',
+        'home'      => 'Chi sono io',
+        'projects'  => 'Progetti',
+        'contact'   => 'Contatto',
+        'technologies' => 'Tecnologie',
+        'linkedin'  => 'Visita il mio LinkedIn',
     ]
 ];
 
@@ -43,7 +57,6 @@ $biography = $translations[$language]['biografia'];
             padding: 0;
             background-color: #222222;
             color: white;
-            /* overflow-x: hidden; */
         }
     </style>
 </head>
@@ -72,22 +85,55 @@ $biography = $translations[$language]['biografia'];
                     </p>
                 </div>
                 <div class="image-block">
-                    <img src="img/dieni.jpg" alt="Foto de perfil de Dienifer" class="profile-image">
-                </div>
-            </div>
-            <div class="technologies">
-                <div class="tech-icons">
-                    <img src="img/html_.png" alt="HTML5" class="tech-icon">
-                    <img src="img/css_.png" alt="CSS3" class="tech-icon">
-                    <img src="img/javascript_.png" alt="JavaScript" class="tech-icon">
-                    <img src="img/php_.png" alt="PHP" class="tech-icon">
-                    <img src="img/mysql_.png" alt="MySQL" class="tech-icon">
+                    <img src="img/dieni_foto.jpg" alt="Foto de perfil de Dienifer" class="profile-image">
                 </div>
             </div>
         </section>
+        <section class="section" id="tech">
+            <div class="technologies">
+                <div class="tech-item" id="php">
+                    <img src="img/php_icon.png" alt="PHP" class="tech-icon">
+                    <div class="tech-info">
+                        <h2>Laravel</h2>
+                        <h2>Bootstrap</h2>
+                    </div>
+                </div>
+                <div class="tech-item" id="javascript">
+                    <img src="img/javascript_icon.png" alt="JAVASCRIPT" class="tech-icon">
+                    <div class="tech-info">
+                        <h2>Jquery</h2>
+                    </div>
+                </div>
+                <div class="tech-item" id="css">
+                    <img src="img/css_icon.png" alt="CSS" class="tech-icon">
+                    <div class="tech-info">
+                        <h2>Tailwindcss</h2>
+                    </div>
+                </div>
+                <div class="tech-item" id="html">
+                    <img src="img/html_icon.png" alt="HTML5" class="tech-icon">
+                </div>
+                <div class="tech-item" id="mysql">
+                    <img src="img/mysql_icon.png" alt="MYSQL" class="tech-icon">
+                    <div class="tech-info">
+                        <h2>Workbench</h2>
+                        <h2>HeidiSQL</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="download-cv-content">
+                <a href="files/curriculum_italy.pdf" download="curriculum_dieniferM" class="btn-download">Download CV</a>
+            </div>
+            <!-- <div class="download-cv-content">
+                <a href="files/Currículo_br.pdf" download="curriculum_dieniferM" class="btn-download">Download CV</a>
+            </div> -->
+            <!-- <div class="download-cv-content">
+                <a href="files/curriculo_ingles.pdf" download="curriculum_dieniferM" class="btn-download">Download CV</a>
+            </div> -->
+        </section>
+
         <section class="section" id="projects">
-            <h2 id="procts">Projects</h2>
-            <p id="my_projects">My projects</p>
+            <h2 id="procts"><?= $translations[$language]['projects'] ?></h2>
             <div class="projects-grid">
                 <div class="project-item">
                     <div class="slider">
@@ -111,7 +157,6 @@ $biography = $translations[$language]['biografia'];
                         <img src="" alt="Project 3 Image 1" class="slide active">
                         <img src="" alt="Project 3 Image 2" class="slide">
                         <img src="" alt="Project 3 Image 3" class="slide">
-                        <!-- Mensagem para projetos sem imagens -->
                         <p class="no-project-message">No project yet</p>
                     </div>
                     <a href="" class="project-link">View Project</a>
@@ -121,19 +166,28 @@ $biography = $translations[$language]['biografia'];
                         <img src="" alt="Project 4 Image 1" class="slide active">
                         <img src="" alt="Project 4 Image 2" class="slide">
                         <img src="" alt="Project 4 Image 3" class="slide">
-                        <!-- Mensagem para projetos sem imagens -->
                         <p class="no-project-message">No project yet</p>
                     </div>
                     <a href="" class="project-link">View Project</a>
                 </div>
-
             </div>
         </section>
-
         <section class="section" id="contact">
-            <h2>E-mail</h2>
-            <p>dienifermendonca.tech@gmail.com</p>
-            <!-- <button class="cta-button">Entre em Contato</button> -->
+            <div class="contact-info">
+                <div class="contact-item">
+                    <a href="mailto:dienifermendonca.tech@gmail.com">
+                        <img src="img/icon_email.png" alt="Ícone de E-mail" class="contact-icon">
+                    </a>
+                    <p><a href="mailto:dienifermendonca.tech@gmail.com">dienifermendonca.tech@gmail.com</a></p>     
+                </div>
+                <div class="contact-item">
+                <a href="https://www.linkedin.com/in/dienifer-dm" target="_blank" id="linkedin-link">
+                    <img src="img/icon_linkedin.png" alt="Ícone de LinkedIn" class="contact-icon">
+                </a>
+                <p><a href="https://www.linkedin.com/in/dienifer-dm" target="_blank" id="linkedin-text"><?= $translations[$language]['linkedin']?></a></p>
+            </div>
+
+            </div>
         </section>
     </div>
     <script>
@@ -197,10 +251,15 @@ $biography = $translations[$language]['biografia'];
         var translations = <?php echo json_encode($translations); ?>;
 
         function changeLanguage(language) {
-            // Verifica se o idioma existe nas traduções
             if (translations[language]) {
-                // Atualiza o conteúdo da biografia de acordo com o idioma selecionado
-                document.getElementById('biografia').textContent = translations[language]['biografia'];
+                document.getElementById('biografia').textContent        = translations[language]['biografia'];
+                document.getElementById('home-link').textContent        = translations[language]['home'];
+                document.getElementById('projects-link').textContent    = translations[language]['projects'];
+                document.getElementById('contact-link').textContent     = translations[language]['contact'];
+                document.getElementById('tech-link').textContent        = translations[language]['technologies'];
+                document.getElementById('linkedin-text').textContent    = translations[language]['linkedin'];
+
+                document.getElementById('procts').textContent           = translations[language]['projects'];
             }
         }
     </script>
--- menu.php
+++ menu.php
@@ -1,8 +1,9 @@
 <header class="menu">
     <ul>
-        <li><a href="#home" class="nav-link">Home</a></li>
-        <li><a href="#projects" class="nav-link">Projects</a></li>
-        <li><a href="#contact" class="nav-link">Contact</a></li>
+        <li><a href="#home" class="nav-link" id="home-link"><?php echo $translations[$language]['home']; ?></a></li>
+        <li><a href="#tech" class="nav-link" id="tech-link"><?php echo $translations[$language]['technologies']; ?></a></li>
+        <li><a href="#projects" class="nav-link" id="projects-link"><?php echo $translations[$language]['projects']; ?></a></li>
+        <li><a href="#contact" class="nav-link" id="contact-link"><?php echo $translations[$language]['contact']; ?></a></li>
     </ul>
     <div class="language-selector">
         <select id="language" onchange="changeLanguage(this.value)">
